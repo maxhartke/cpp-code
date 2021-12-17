78dba207d1381b2ee5b26a0a44d3c45611e7f735
Max Hartke <56495123+maxhartke@users.noreply.github.com>
2021-12-17 15:37:37 -0600

    Update test.s

--- cs2640/test.s
+++ cs2640/test.s
@@ -1,72 +1,76 @@
 #
 #   Name: Hartke, Max
-#   Homework: #2
-#   Due: March 12, 2021
+#   Project: #4
+#   Due: April 30, 2021 by 10 am
 #   Course: cs-2640-01-sp21
 #
 #   Description:
-#               Program to compute and output the count and the average.
+#               Program that computes and returns x^y using the pow recursive function
 #
         .data
-title:  .asciiz "Sum by M. Hartke\n\nEnter data, use 0 to end input.\n\n"
-prompt: .asciiz "? "
-sumis:  .asciiz "\nThe sum is "
-count:  .asciiz "\nThe count is "
-avrg:   .asciiz "\nThe average is "
-null:   .asciiz "\nNo data entered\n"
+title:  .asciiz "Pow by M. Hartke\n"
+descpt: .asciiz "n.....2^n\n"
+dots:   .asciiz "....."
+x = 2 
+
         .text
 main:
-        la      $a0, title      # output program heading
-        li      $v0, 4
+        la	$a0, title      # display title
+        li	$v0, 4
         syscall
-
-        li      $t0, 0          # sum:t0
-        li      $t1, 0          # count: t1
+        li      $a0, '\n'       # new line
+        li      $v0, 11
+        syscall
+        la	$a0, descpt     # display description
+        li	$v0, 4
+        syscall
+        li      $t0, 0          # t0: y
 while:
-        la      $a0, prompt     # display prompt
-        li      $v0, 4
-        syscall
-        la      $v0, 5          # read data
-        syscall
-        beqz    $v0, endw       # if data == 0, done
-
-        add     $t0, $t0, $v0   # add input to sum
-        add     $t1, $t1, 1     # incrment count
-        b       while
-endw:
-        beqz    $t1, empty      # if count == 0, No data entered
-        la      $a0, sumis      # output sum text
-        li      $v0, 4
-        syscall
-        move    $a0, $t0        # output sum
+        move    $a0, $t0        # print out y
         li      $v0, 1
         syscall
-        la      $a0, count      # output count text
-        li      $v0, 4
+        la	$a0, dots       # display dots
+        li	$v0, 4
         syscall
-        move    $a0, $t1        # output count
-        li      $v0, 1
-        syscall
-        la      $a0, avrg       # output average text
-        li      $v0, 4
-        syscall
-        div    $a0, $t0, $t1    # output average
+        li      $a0, x          # a0: x
+        move    $a1, $t0        # a1: y
+        jal     pow             # call pow
+        move    $a0, $v0        # print out x^y
         li      $v0, 1
         syscall
         li      $a0, '\n'       # new line
         li      $v0, 11
         syscall
-empty:
-        bnez    $t1, exit       # if count != 0, exit
-        la      $a0, null       # output null text
-        li      $v0, 4
+        addi    $t0, $t0, 1     # increment count
+        bne     $t0, 10, while  # if < 10 then while 
+exit: 
+        li      $v0, 10         # exit
         syscall
-        li      $a0, '\n'       # new line
-        li      $v0, 11
-        syscall
-
-exit:   li      $v0, 10         # exit
-        syscall
-
-
-        .end    main
+        .end    main 
+#
+#       int pow(int x, int y)
+#               returns x * pow(x, y -1)
+#       Parameters:
+#               a0: int x
+#               a1: int y
+#       Return value:
+#               v0: x * pow(x, y -1)
+#
+pow:
+        addiu   $sp, $sp, -16   # allocate space on stack
+        sw      $ra, 12($sp)
+        sw      $a0, 8($sp)
+        sw      $a1, 4($sp)
+        beqz    $a1, rtrn1      # if y = 0 then rtrn1
+        addiu   $a1, $a1, -1    # subtract 1 from y
+        jal     pow             # call pow
+        lw      $a0, 8($sp)        
+        lw      $a1, 4($sp)  
+        mul     $v0, $v0, $a0   # multiply x by return value of pow
+rtrn:   
+        lw      $ra, 12($sp)
+        addiu   $sp, $sp, 16
+        jr      $ra             # return to main
+rtrn1:                                        
+        li      $v0, 1          # return 1
+        b       rtrn            # return to rttn
